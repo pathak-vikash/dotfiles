c1a23eafd6586085bb2d733bce7297032e47655c
Vikash pathak <vikashh.pathak@gmail.com>
2020-02-18 02:59:56 +0530

    Removed unused commands.

--- aliases.zsh
+++ aliases.zsh
@@ -28,24 +28,14 @@ alias a="php artisan"
 alias ams="php artisan migrate:fresh --seed"
 
 # PHP
-alias php73="/usr/local/Cellar/php@7.3/7.3.13/bin/php"
-alias php72="/usr/local/Cellar/php@7.2/7.2.26/bin/php"
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
 
-# Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
-
 # Docker
-alias dcomposer="docker-compose"
+alias dcompose="docker-compose"
 #alias dstop="docker stop $(docker ps -a -q)"
 #alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
 #alias dpurgeimages="docker rmi $(docker images -q)"
