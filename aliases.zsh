7396702725f24e8e88a3912e6f460ea240c93e3e
Vikash pathak <vikashh.pathak@gmail.com>
2020-02-18 02:06:25 +0530

    Added aliases file.

---
+++ aliases.zsh
@@ -0,0 +1,56 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias reloadcli="source $HOME/.zshrc"
+
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias sites="cd $HOME/Sites"
+alias lara="sites && cd laravel/"
+
+# Laravel
+alias a="php artisan"
+alias ams="php artisan migrate:fresh --seed"
+
+# PHP
+alias php73="/usr/local/Cellar/php@7.3/7.3.13/bin/php"
+alias php72="/usr/local/Cellar/php@7.2/7.2.26/bin/php"
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run watch"
+
+# Vagrant
+alias v="vagrant global-status"
+alias vup="vagrant up"
+alias vhalt="vagrant halt"
+alias vssh="vagrant ssh"
+alias vreload="vagrant reload"
+alias vrebuild="vagrant destroy --force && vagrant up"
+
+# Docker
+alias dcomposer="docker-compose"
+#alias dstop="docker stop $(docker ps -a -q)"
+#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+#alias dpurgeimages="docker rmi $(docker images -q)"
+#dbuild() { docker build -t=$1 .; }
+#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+
+# Git
+alias commit="git add . && git commit -m"
+alias gcommit="git add . && git commit"
+alias amend="git add . && git commit --amend --no-edit"
+alias wip="commit wip"
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gd="git diff"
+alias resolve="git add . && git commit --no-edit"
+alias gl="git log --oneline --decorate --color"
+alias nuke="git clean -df && git reset --hard"
+alias unstage="git restore --staged ."
